971f825cb62d3b8b70abc64faef3504f83b527d3
snehakr <snehakr044@gmail.com>
2020-04-01 19:28:44 +0530

    1/4/20

--- src/pages/roughInvCreate.js
+++ src/pages/roughInvCreate.js
@@ -4,150 +4,198 @@ import DatePicker from 'react-date-picker';
 
 class App extends Component {
 
-    state = {
-        data: [],
-        title:'Table',
-        date: new Date(),
-        arr:[
-            {items:'Item1', Kg:10, Box:10, Total:100},
-            {items:'Item1', Kg:10, Box:10, Total:100}
-        ] 
+  state = {
+    data: [],
+    kg:'',
+    box:'',
+    total:'',
+    title: 'Table',
+    date: new Date(),
+    arrItems: [
+      { Id_item:1, Items: 'Item 1' },
+      { Id_item:2, Items: 'Item 2' },
+      { Id_item:3, Items: 'Item 3' },
+    ]
+  }
 
-      }
-     
-      onChange = date => this.setState({ date })
-     
 
-    componentDidMount() {
-        
-    }
+  onChange = date => this.setState({ date })
 
-    componentWillMount() {
+  handleChangeKg = async function(e)  {
+    await this.setState({
+       kg: e.target.value
+    })
+    this.calcTotal();
+}
 
-    }
+handleChangeBox = async function(e) {
 
-    render() {
-   
+  await this.setState({
+      box: e.target.value
+    })
+    this.calcTotal();
+  }
+    calcTotal(){
+    
+    if(this.state.kg=="" || this.state.box=="") return
+    else    
+    var kg_ = this.state.kg;
+    var box_ = this.state.box;
+    var tot = (kg_ * box_);
+    
+    this.setState({total:tot})
+  }
+
+  calcGrandTotal(){
+    //for(var i=0; i<)
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+
     return (
-        <div class="wrapper" >
-            <Nav />
-            <div class="content-wrapper">
+      <div class="wrapper" >
+        <Nav />
+        <div class="content-wrapper">
 
-                <section class="content-header">
-                    <div class="container-fluid">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1>Rough Invoice</h1>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <div class="content">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-12">
-
-                                <div class="card card-success">
-                                    <div class="card-header">
-                                        <h3 class="card-title">New Rough Invoice</h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <form >
-                                            <div class="row">
-                                                <div class="col-sm-12">
-                                                    <div class="form-group">
-                                                        <label>Date:</label>
-
-                                                        <div class="input-group">
-                                                            <div class="input-group-prepend">
-                                                                <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
-                                                            </div>
-                                                            <DatePicker
-                                                                className={"form-control"}
-                                                                onChange={this.onChange}
-                                                                value={this.state.date}
-                                                                format={"dd/MM/yyyy"}
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-sm-12">
-
-                                                    <div class="form-group">
-                                                        <label>Part of loading</label>
-                                                        <select class="form-control">
-                                                            <option>Cochin</option>
-                                                            <option>Calicut</option>
-                                                            <option>Thrissur</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="row">
-                                                <div class="col-sm-12">
-
-                                                    <div class="form-group">
-                                                        <label>Consigner</label>
-                                                        <textarea class="form-control" rows="3" placeholder="Consigner"></textarea>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-sm-12">
-                                                    <div class="form-group">
-                                                        <label>Consignee</label>
-                                                        <textarea class="form-control" rows="3" placeholder="Consignee" ></textarea>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                        </form>
-                                    </div>
-                                    
-                                    <div class="card-header">
-                                        <h3 class="card-title">Invoice</h3>
-                                    </div>
-                                    <div class="card-body p-0">
-                                        <table class="table">
-                                            <thead>
-                                                <tr>
-                                                    <th style={{ width: '10px' }}>No</th>
-                                                    <th>Item</th>
-                                                    <th style={{ width: '10%' }}>Kg</th>
-                                                    <th style={{ width: '10%' }}>Box</th>
-                                                    <th style={{ width: '10%' }}>Total</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-
-                                           
-                                            <tr>
-
-                                            {this.state.arr.map((d) => <ul><li key={d.items}>{d.items}</li><li key={d.Kg}>{d.Kg}</li></ul>)}
-
-                                                
-                                                   
-                                                </tr>
-                                               
-                                            </tbody>
-                                        </table>
-                                       
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+          <section class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1>Rough Invoice</h1>
                 </div>
+              </div>
             </div>
+          </section>
 
+          <div class="content">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-lg-12">
+
+                  <div class="card card-success">
+                    <div class="card-header">
+                      <h3 class="card-title">New Rough Invoice</h3>
+                    </div>
+                    <div class="card-body">
+                      <form >
+                        <div class="row">
+                          <div class="col-sm-12">
+                            <div class="form-group">
+                              <label>Date:</label>
+
+                              <div class="input-group">
+                                <div class="input-group-prepend">
+                                  <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                                </div>
+                                <DatePicker
+                                  className={"form-control"}
+                                  onChange={this.onChange}
+                                  value={this.state.date}
+                                  format={"dd/MM/yyyy"}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-sm-12">
+
+                            <div class="form-group">
+                              <label>Part of loading</label>
+                              <select class="form-control">
+                                <option>Cochin</option>
+                                <option>Calicut</option>
+                                <option>Thrissur</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-sm-12">
+
+                            <div class="form-group">
+                              <label>Consigner</label>
+                              <textarea class="form-control" rows="3" placeholder="Consigner"></textarea>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-sm-12">
+                            <div class="form-group">
+                              <label>Consignee</label>
+                              <textarea class="form-control" rows="3" placeholder="Consignee" ></textarea>
+                            </div>
+                          </div>
+                        </div>
+
+                      </form>
+                    </div>
+
+                    <div class="card-header">
+                      <h3 class="card-title">Invoice</h3>
+                    </div>
+                    <div class="card-body p-0">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                            <th>Item</th>
+                            <th style={{ width: '10%' }}>Kg</th>
+                            <th style={{ width: '10%' }}>Box</th>
+                            <th style={{ width: '10%' }}>Total</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+
+                          <tr>
+                            <td><select class="form-control">
+                               {this.state.arrItems.map((column) => <option value={column.Id_item}>{column.Items}</option>)}
+                                </select>
+                            </td>
+                                
+                            <td><input type="text" class="form-control" value={this.state.kg} onChange={(e) => this.handleChangeKg(e)} /></td>
+                            <td><input type="text" class="form-control"  value={this.state.box}  onChange={(e) => this.handleChangeBox(e)}  /></td>
+                            <td>{this.state.total}</td>
+                          </tr>
+                          <tr>
+                            <td><select class="form-control">
+                               {this.state.arrItems.map((column) => <option value={column.Id_item}>{column.Items}</option>)}
+                                </select>
+                            </td>
+                                
+                            <td><input type="text" class="form-control" value={this.state.kg} onChange={(e) => this.handleChangeKg(e)} /></td>
+                            <td><input type="text" class="form-control"  value={this.state.box}  onChange={(e) => this.handleChangeBox(e)}  /></td>
+                            <td>{this.state.total}</td>
+                          </tr>
+                        </tbody>
+                        <tfoot>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>Hi</td>
+                        </tfoot>
+                      </table>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
         </div>
+
+      </div>
     );
-    }
+  }
 }
 
 export default App;

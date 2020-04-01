18935e98bcf373543bf1330d6686fa7eaa9c2a07
AbdulQadhir <kader.chalil@gmail.com>
2020-04-02 00:04:49 +0530

    Rough Invoice: Table

--- src/pages/roughInvCreate.js
+++ src/pages/roughInvCreate.js
@@ -4,79 +4,83 @@ import DatePicker from 'react-date-picker';
 
 class App extends Component {
 
-  state = {
-    title: 'Table',
-    date: new Date(),
-    arrItems: [
-      { Id_item:1, Items: 'Item 1' },
-      { Id_item:2, Items: 'Item 2' },
-      { Id_item:3, Items: 'Item 3' },
-    ],
+  constructor(props) {
+    super(props);
     
-    invItems: [
-      { id_item:1, kg : 10, box: 100 },
-      { id_item:2, kg : 10, box: 100 },
-      { id_item:3, kg : 10, box: 100 },
-    ]
+    this.state = {
+      title: 'Table',
+      date: new Date(),
+      products: [
+        { Id_item:0, Items: '--Select--' },
+        { Id_item:1, Items: 'Item 1' },
+        { Id_item:2, Items: 'Item 2' },
+        { Id_item:3, Items: 'Item 3' },
+      ],
+      invItems: [
+        { id_item:1, kg : 10, box: 100 },
+        { id_item:2, kg : 20, box: 200 },
+        { id_item:3, kg : 30, box: 300 },
+        { id_item:0, kg : "", box: "" },
+      ]
+    }
   }
 
-  onChange = date => this.setState({ date })
+  onDateChange = date => this.setState({ date })
 
-  handleChangeKg = async function(e)  {
-    await this.setState({
-       kg: e.target.value
+  handleChangeKg = (e, rowIndex) =>  {
+      let _invItems = this.state.invItems;
+      let _row = _invItems[rowIndex];
+      _row.kg = e.target.value;
+      _invItems[rowIndex] = _row;
+      this.setState({
+        invItems: _invItems
+      })
+  }
+
+  handleChangeBox = (e, rowIndex) =>  {
+    let _invItems = this.state.invItems;
+    let _row = _invItems[rowIndex];
+    _row.box = e.target.value;
+    _invItems[rowIndex] = _row;
+    this.setState({
+      invItems: _invItems
     })
-    this.calcTotal();
-}
+  }
 
-handleChangeBox = async function(e) {
-
-  await this.setState({
-      box: e.target.value
+  handleChangeProduct = (e, rowIndex) =>  {
+    let _invItems = this.state.invItems;
+    let _row = _invItems[rowIndex];
+    _row.id_item = e.target.value;
+    _invItems[rowIndex] = _row;
+    this.setState({
+      invItems: _invItems
     })
-    this.calcTotal();
-  }
-    calcTotal(){
-    
-    if(this.state.kg=="" || this.state.box=="") return
-    else    
-    var kg_ = this.state.kg;
-    var box_ = this.state.box;
-    var tot = (kg_ * box_);
-    
-    this.setState({total:tot})
-  }
-
-  calcGrandTotal(){
-    
-  }
-
-  componentDidMount() {
-
-  }
-
-  componentWillMount() {
-
   }
   
+  delRow = (rowIndex) =>  {
+    let _invItems = this.state.invItems;
+    _invItems.splice(rowIndex, 1);
+    this.setState({
+      invItems: _invItems
+    })
+  }
+
   render() {
+    const tableRows = this.state.invItems.map((invItem,index) =>
+      <TableRow 
+        handleChangeKg={this.handleChangeKg}
+        handleChangeBox={this.handleChangeBox} 
+        handleChangeProduct={this.handleChangeProduct} 
+        delRow={this.delRow} 
+        invItem={invItem} rowIndex={index} 
+        products={this.state.products}
+        />
+  );
 
-    const tableRows = this.state.invItems.map((invItem) =>
-      <tableRow items={invItem} />
-    );
-    
-    const tableRow = (props) => (
-      <tr>
-        <td>
-          
-        </td>
-            
-        <td><input type="text" class="form-control" value={this.state.kg} onChange={(e) => this.handleChangeKg(e)} /></td>
-        <td><input type="text" class="form-control"  value={this.state.box}  onChange={(e) => this.handleChangeBox(e)}  /></td>
-        <td>{this.state.total}</td>
-      </tr>
-    );
-
+    const grandTotal = this.state.invItems.reduce((a, b) => +a + +(b.kg*b.box), 0);
+    const boxTotal = this.state.invItems.reduce((a, b) => +a + +(b.box), 0);
+    const kgTotal = this.state.invItems.reduce((a, b) => +a + +(b.kg), 0);
+  
     return (
       <div class="wrapper" >
         <Nav />
@@ -97,7 +101,7 @@ handleChangeBox = async function(e) {
               <div class="row">
                 <div class="col-lg-12">
 
-                  <div class="card card-success">
+                  <div class="card card-info">
                     <div class="card-header">
                       <h3 class="card-title">New Rough Invoice</h3>
                     </div>
@@ -114,7 +118,7 @@ handleChangeBox = async function(e) {
                                 </div>
                                 <DatePicker
                                   className={"form-control"}
-                                  onChange={this.onChange}
+                                  onChange={this.onDateChange}
                                   value={this.state.date}
                                   format={"dd/MM/yyyy"}
                                 />
@@ -122,9 +126,9 @@ handleChangeBox = async function(e) {
                             </div>
                           </div>
                         </div>
+
                         <div class="row">
                           <div class="col-sm-12">
-
                             <div class="form-group">
                               <label>Part of loading</label>
                               <select class="form-control">
@@ -145,6 +149,7 @@ handleChangeBox = async function(e) {
                             </div>
                           </div>
                         </div>
+
                         <div class="row">
                           <div class="col-sm-12">
                             <div class="form-group">
@@ -168,21 +173,20 @@ handleChangeBox = async function(e) {
                             <th style={{ width: '10%' }}>Kg</th>
                             <th style={{ width: '10%' }}>Box</th>
                             <th style={{ width: '10%' }}>Total</th>
+                            <th style={{ width: '10%' }}></th>
                           </tr>
                         </thead>
                         <tbody>
-                          {
-                            tableRows
-                          }
+                          { tableRows }
                         </tbody>
                         <tfoot>
                           <td></td>
+                          <td>{ kgTotal }</td>
+                          <td>{ boxTotal }</td>
+                          <td align="right" >{ grandTotal }</td>
                           <td></td>
-                          <td></td>
-                          <td>Hi</td>
                         </tfoot>
                       </table>
-
                     </div>
                   </div>
                 </div>
@@ -195,6 +199,45 @@ handleChangeBox = async function(e) {
       </div>
     );
   }
+}
+
+class TableRow extends React.Component {
+
+	handleChangeKg = (e) => {
+    this.props.handleChangeKg(e, this.props.rowIndex);
+  }
+  
+	handleChangeBox = (e) => {
+    this.props.handleChangeBox(e, this.props.rowIndex);
+  }
+  
+	handleChangeProduct = (e) => {
+    this.props.handleChangeProduct(e, this.props.rowIndex);
+  }
+
+	delRow = () => {
+    this.props.delRow(this.props.rowIndex);
+  }
+
+	render() {
+    let invItem = this.props.invItem;
+    let total = invItem.kg * invItem.box;
+		return (
+      <tr>
+        <td>
+          <select class="form-control"  onChange={(e) => this.handleChangeProduct(e)} value={invItem.id_item} >
+            {this.props.products.map((column) => <option value={column.Id_item}>{column.Items}</option>)}
+          </select>
+        </td>
+        <td><input type="text" class="form-control" value={invItem.kg} onChange={(e) => this.handleChangeKg(e)} /></td>
+        <td><input type="text" class="form-control"  value={invItem.box}  onChange={(e) => this.handleChangeBox(e)}  /></td>
+        <td align="right" >{total}</td>
+        <td>
+            <button type="button"  onClick={this.delRow}  class="btn btn-success"><i class="fas fa-trash"></i></button>
+        </td>
+      </tr>
+    );
+	}
 }
 
 export default App;

d4f7e934da5f28e601ee943161ac28884a439999
AbdulQadhir <kader.chalil@gmail.com>
2020-04-05 11:24:41 +0530

    Invoice

--- src/pages/invoice.js
+++ src/pages/invoice.js
@@ -413,11 +413,11 @@ class Documents extends Component {
 	render() {
 		return (
       <div >
-      <section class="content">
-        <div class="container-fluid">
-         Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam. 
-        </div>
-      </section>
+        <section class="content">
+          <div class="container-fluid">
+          Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam. 
+          </div>
+        </section>
       </div>
     );
   }

e297e4689ec9f1233c8d70784dea091041f6d63f
snehakr <snehakr044@gmail.com>
2020-04-06 13:09:45 +0530

    docs - 4/6 noon

---
+++ src/pages/invoice/documents.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-date-picker';
+
+
+
+class Documents extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            remark:'',
+            doc:'',
+            arrDocs:[
+                {id_doc:'0', doc:'--Select'},
+                {id_doc:'1', doc:'Document 1'},
+                {id_doc:'2', doc:'Document 2'},
+                {id_doc:'3', doc:'Document 3'},
+            ],
+            arrTable:[]
+        }
+        this.onChangeDoc    =this.onChangeDoc.bind(this);
+        this.onChangeRemark =this.onChangeRemark.bind(this);
+        this.onAddClick     =this.onAddClick.bind(this)
+    }
+    onChangeDoc(event){
+        this.setState({doc:event.target.value})
+    }
+
+    onChangeRemark(event){
+        this.setState({remark:event.target.value})
+    }
+
+    onAddClick(e){
+        e.preventDefault();
+        let data_=this.state.arrTable;
+        data_.push({
+            id_doc:this.state.doc,
+            remark:this.state.remark
+        })
+
+        this.setState({
+            arrTable:data_,
+            remark:'',
+            id_doc:0
+        })
+            
+    }
+ 
+    render(){
+       
+        const tableRows =this.state.arrTable.map((tbl) => 
+         <TableRow 
+         tbl={tbl}
+         arrDocs={this.state.arrDocs}
+         />
+        )
+        return(
+             <div >
+
+        <div class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="card card-info">
+                  <div class="card-body p-0">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th style={{ width: '40%' }}>
+                            <select class="form-control" onChange={this.onChangeDoc}>
+                              {this.state.arrDocs.map((docs) =>
+                                <option value={docs.id_doc}>{docs.doc}</option>)}
+                            </select>
+                          </th>
+                          <th style={{ width: '30%' }}>
+                            <input type="text" onChange={this.onChangeRemark} value={this.state.remark}  class="form-control" />
+                          </th>
+                          <th> 
+                              <button type="submit" class="btn btn-default">Select File
+                              </button>                     
+                          </th>
+                          <th>
+                            <button type="button"  class="btn btn-success" onClick={(e) => this.onAddClick(e)}>
+                              <i class="fas fa-plus"></i>
+                            </button>
+
+                          </th>
+                        </tr>
+                        <tr>
+                          <th style={{ width: '25%' }}>Documents</th>
+                          <th style={{ width: '15%' }}>Remarks</th>
+                          <th></th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {tableRows}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+        )
+    }
+}
+
+class TableRow extends Component{
+    
+  delRow = () => {
+    this.props.delRow(this.props.rowIndex);
+  }
+
+    render(){
+        let tbl=this.props.tbl;
+        let doc = (id_doc) => {
+            return this.props.arrDocs.filter(function (el) {
+              return el.id_doc == id_doc;
+            })[0].doc;
+          }
+
+        return(
+            <tr>
+                <td>{doc(tbl.id_doc)} </td>
+                <td>{tbl.remark}</td>
+                <td>
+                    <button type="button" onClick={this.delRow} class="btn btn-success">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                </td>
+                <td>
+                    <button type="button" onClick={this.delRow} class="btn btn-success">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </td>
+
+            </tr>
+        );
+    }
+}
+
+export default  Documents;
--- src/pages/invoice/index.js
+++ src/pages/invoice/index.js
@@ -3,6 +3,7 @@ import Nav from '../../NavBar';
 import DatePicker from 'react-date-picker';
 import Expenses from './expense'
 import NetReport from './netreport'
+import Documents from './documents'
 
 class Invoice extends Component {
 
@@ -409,18 +410,5 @@ class App extends Component {
     );
   }
 }
-
-
-class Documents extends Component {
-
-	render() {
-		return (
-      <div >
-         Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam. 
-      </div>
-    );
-  }
-}
-
 
 export default App;

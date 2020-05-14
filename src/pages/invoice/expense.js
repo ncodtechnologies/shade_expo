475d5f450266b0209a2d3eca3eb988c7c76aa33d
AbdulQadhir <kader.chalil@gmail.com>
2020-05-14 05:53:48 +0530

    tmp

--- src/pages/invoice/expense.js
+++ src/pages/invoice/expense.js
@@ -8,20 +8,20 @@ class Expense extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data:null,
+      data: null,
       date: new Date(),
-      id_ledger_from:'',
-      id_ledger_to:'',
-      description:'',
-      rate:'',
-      amount: '',
-      type:'Payment',
-      voucher_no:'1',
-      ledger: '',
+      id_ledger_from: "",
+      id_ledger_to: "",
+      description: "",
+      rate: "",
+      amount: "",
+      type: "Payment",
+      voucher_no: "1",
+      ledger: "",
       arrLedger: [],
-      arrExpenses: []
-    }
-    
+      arrExpenses: [],
+    };
+
     this.onAmountChange = this.onAmountChange.bind(this);
     this.onRateChange = this.onRateChange.bind(this);
     this.onDescriptionChange = this.onDescriptionChange.bind(this);
@@ -31,98 +31,93 @@ class Expense extends Component {
     this.validator = new SimpleReactValidator();
   }
 
-  
   componentDidMount() {
     const id_invoice = this.props.id_invoice;
-   this.loadAccountHead();
-   this.loadExpenseList(id_invoice);
-   console.log(id_invoice)
+    this.loadAccountHead();
+    this.loadExpenseList(id_invoice);
+    console.log(id_invoice);
   }
-  loadAccountHead(){
+  loadAccountHead() {
     fetch(URL_LEDGER_DT)
-    .then(response => response.json())
-    .then(data => this.setState({ arrLedger: data }));
+      .then((response) => response.json())
+      .then((data) => this.setState({ arrLedger: data }));
     //console.log(data)
   }
 
   loadExpenseList = (id_invoice) => {
     fetch(URL_EXPENSE_DT + `/${id_invoice}`)
-    .then(response => response.json())
-    .then(data => {
-      if(data.length>0)
-      this.setState({
-         arrExpenses: data ,
-        })
-        }
-      );
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0)
+          this.setState({
+            arrExpenses: data,
+          });
+      });
     //console.log(data)
-  }
+  };
 
   saveInvoice = () => {
     if (this.validator.allValid()) {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
-                date          : new Date().toISOString().slice(0, 10),
-                id_ledger_from: this.state.id_ledger_from ,
-                id_ledger_to  : this.state.id_ledger_to ,
-                description   : this.state.description ,
-                rate          : this.state.rate ,
-                amount        : this.state.amount ,
-                type          : this.state.type ,
-                voucher_no    : this.state.voucher_no ,
-                id_invoice    : this.props.id_invoice,
-              })
-  };
-  fetch(URL_EXPENSE_SAVE, requestOptions)
-      .then(response => response.json());
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          date: new Date().toISOString().slice(0, 10),
+          id_ledger_from: this.state.id_ledger_from,
+          id_ledger_to: this.state.id_ledger_to,
+          description: this.state.description,
+          rate: this.state.rate,
+          amount: this.state.amount,
+          type: this.state.type,
+          voucher_no: this.state.voucher_no,
+          id_invoice: this.props.id_invoice,
+        }),
+      };
+      fetch(URL_EXPENSE_SAVE, requestOptions).then((response) =>
+        response.json()
+      );
       this.loadExpenseList(this.props.id_invoice);
-} 
-    else
-    {
-    this.validator.showMessages();
-    this.forceUpdate();
+    } else {
+      this.validator.showMessages();
+      this.forceUpdate();
     }
-}
-  onDateChange = date => this.setState({ date })
+  };
+  onDateChange = (date) => this.setState({ date });
 
-  delRow = (rowIndex) => {
-    let _arrExpenses = this.state.arrExpenses;
-    _arrExpenses.splice(rowIndex, 1);
-    this.setState({
-      arrExpenses: _arrExpenses
-    })
-  }
+  onDelRow = (id) => {
+    alert(id);
+    this.setState({ description: id });
+  };
 
   onDescriptionChange(event) {
-    this.setState({ description: event.target.value })
+    this.setState({ description: event.target.value });
   }
-  
+
   onRateChange(event) {
-    this.setState({ rate: event.target.value })
+    this.setState({ rate: event.target.value });
   }
 
   onAmountChange(event) {
-    this.setState({ amount: event.target.value })
+    this.setState({ amount: event.target.value });
   }
 
   onLedgerFromChange(event) {
-    this.setState({ id_ledger_from: event.target.value })
+    this.setState({ id_ledger_from: event.target.value });
   }
 
   onLedgerToChange(event) {
-    this.setState({ id_ledger_to: event.target.value })
+    this.setState({ id_ledger_to: event.target.value });
   }
- 
-  render() {
-    const tableRows = this.state.arrExpenses.map((arrExpense, index) =>
-      <TableRow
-        arrExpense={arrExpense}
-        arrLedger = {this.state.arrLedger}
-      />);
 
-    const grandTotal = this.state.arrExpenses.reduce((a, b) => +a + +(b.amount), 0);
+  render() {
+    const tableRows = this.state.arrExpenses.map((arrExpense, index) => (
+      <TableRow arrExpense={arrExpense} arrLedger={this.state.arrLedger} onDelRow={this.onDelRow} />
+    ));
+
+    const grandTotal = this.state.arrExpenses.reduce(
+      (a, b) => +a + +b.amount,
+      0
+    );
 
     return (
       <div>
@@ -135,97 +130,137 @@ class Expense extends Component {
                     <table class="table">
                       <thead>
                         <tr>
-                          <th colspan={6} >
-                            <div class="row" >
+                          <th colspan={6}>
+                            <div class="row">
                               <div class="col-sm-4">
-                                  <div class="form-group">
-                                    <label>Date</label>
-                                    <DatePicker
-                                      className={"form-control"}
-                                      onChange={this.onDateChange}
-                                      value={this.state.date}
-                                      format={"dd/MM/yyyy"}
-                                    />
-                                  </div>
+                                <div class="form-group">
+                                  <label>Date</label>
+                                  <DatePicker
+                                    className={"form-control"}
+                                    onChange={this.onDateChange}
+                                    value={this.state.date}
+                                    format={"dd/MM/yyyy"}
+                                  />
+                                </div>
                               </div>
                               <div class="col-sm-4">
-                                  <div class="form-group">
-                                    <label>From</label>
-                                    <select class="form-control" onChange={this.onLedgerFromChange} value={this.state.id_ledger_from}>
-                                      {this.state.arrLedger.map((ledger) =>
-                                        <option value={ledger.id_account_head}>{ledger.account_head}</option>)}
-                                    </select>
-                                    {this.validator.message('id_ledger_from', this.state.id_ledger_from, 'required|numeric')}
-                                  </div>
+                                <div class="form-group">
+                                  <label>From</label>
+                                  <select
+                                    class="form-control"
+                                    onChange={this.onLedgerFromChange}
+                                    value={this.state.id_ledger_from}
+                                  >
+                                    {this.state.arrLedger.map((ledger) => (
+                                      <option value={ledger.id_account_head}>
+                                        {ledger.account_head}
+                                      </option>
+                                    ))}
+                                  </select>
+                                  {this.validator.message(
+                                    "id_ledger_from",
+                                    this.state.id_ledger_from,
+                                    "required|numeric"
+                                  )}
+                                </div>
                               </div>
                               <div class="col-sm-4">
-                                  <div class="form-group">
-                                    <label>To</label>
-                                    <select class="form-control" onChange={this.onLedgerToChange} value={this.state.id_ledger_to}>
-                                      {this.state.arrLedger.map((ledger) =>
-                                        <option value={ledger.id_account_head}>{ledger.account_head}</option>)}
-                                    </select>
-                                    {this.validator.message('id_ledger_to', this.state.id_ledger_to, 'required|numeric')}
-                                  </div>
+                                <div class="form-group">
+                                  <label>To</label>
+                                  <select
+                                    class="form-control"
+                                    onChange={this.onLedgerToChange}
+                                    value={this.state.id_ledger_to}
+                                  >
+                                    {this.state.arrLedger.map((ledger) => (
+                                      <option value={ledger.id_account_head}>
+                                        {ledger.account_head}
+                                      </option>
+                                    ))}
+                                  </select>
+                                  {this.validator.message(
+                                    "id_ledger_to",
+                                    this.state.id_ledger_to,
+                                    "required|numeric"
+                                  )}
+                                </div>
                               </div>
                             </div>
                             <div class="row">
                               <div class="col-sm-4">
-                                  <div class="form-group">
-                                    <label>Description</label>
-                                    <input type="text" value={this.state.description} onChange={this.onDescriptionChange} class="form-control" />
-                                  </div>
+                                <div class="form-group">
+                                  <label>Description</label>
+                                  <input
+                                    type="text"
+                                    value={this.state.description}
+                                    onChange={this.onDescriptionChange}
+                                    class="form-control"
+                                  />
+                                </div>
                               </div>
                               <div class="col-sm-4">
-                                  <div class="form-group">
-                                    <label>Rate</label>
-                                    <input type="text" value={this.state.rate} onChange={this.onRateChange} class="form-control" />
-                                  </div>
+                                <div class="form-group">
+                                  <label>Rate</label>
+                                  <input
+                                    type="text"
+                                    value={this.state.rate}
+                                    onChange={this.onRateChange}
+                                    class="form-control"
+                                  />
+                                </div>
                               </div>
                               <div class="col-sm-4">
-                                  <div class="form-group">
-                                    <label>Amount</label>
-                                    <input type="text" value={this.state.amount} onChange={this.onAmountChange} class="form-control" />
-                                    {this.validator.message('amount', this.state.amount, 'required|numeric')}
-                                  </div>
+                                <div class="form-group">
+                                  <label>Amount</label>
+                                  <input
+                                    type="text"
+                                    value={this.state.amount}
+                                    onChange={this.onAmountChange}
+                                    class="form-control"
+                                  />
+                                  {this.validator.message(
+                                    "amount",
+                                    this.state.amount,
+                                    "required|numeric"
+                                  )}
+                                </div>
                               </div>
-                                
-                              
-                                <button type="button"  class="btn btn-block btn-success btn-flat" onClick={this.saveInvoice}>
-                                   Save
-                                </button>
+
+                              <button
+                                type="button"
+                                class="btn btn-block btn-success btn-flat"
+                                onClick={this.saveInvoice}
+                              >
+                                Save
+                              </button>
                             </div>
                           </th>
                         </tr>
                         <tr>
-                          <th style={{ width: '25%' }}>Date</th>
-                          <th style={{ width: '50%' }}>From</th>
-                          <th style={{ width: '25%' }}>To</th>
-                          <th style={{ width: '25%' }}>Description</th>
-                          <th style={{ width: '25%' }}>Amount</th>
-                          <th></th>
+                          <th style={{ width: "25%" }}>Date</th>
+                          <th style={{ width: "50%" }}>From</th>
+                          <th style={{ width: "25%" }}>To</th>
+                          <th style={{ width: "25%" }}>Description</th>
+                          <th style={{ width: "25%" }}>Amount</th>
+                          <th />
                         </tr>
                       </thead>
-                      <tbody>
-                        {tableRows}
-                      </tbody>
+                      <tbody>{tableRows}</tbody>
                       <tfoot>
                         <th>Total</th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th align="right" >{grandTotal}</th>
-                        <th></th>
+                        <th />
+                        <th />
+                        <th />
+                        <th align="right">{grandTotal}</th>
+                        <th />
                       </tfoot>
                     </table>
                   </div>
                 </div>
               </div>
             </div>
-
           </div>
         </div>
-
       </div>
     );
   }
@@ -245,9 +280,9 @@ class TableRow extends React.Component {
         })
         }
       );
+
       //this.loadVoucherList(date);
   }
-
 
   render() {
     let arrExpense = this.props.arrExpense;
@@ -267,7 +302,7 @@ class TableRow extends React.Component {
         <td>{arrExpense.amount}</td>
         <td>
           <div class="btn-group">
-            <button type="button" onClick={() =>this.delExpense(arrExpense.id_account_voucher)} class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+            <button type="button" onClick={() => this.props.onDelRow(arrExpense.id_account_voucher)} class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
           </div>
         </td>
       </tr>

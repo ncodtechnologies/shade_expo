c14b65da4caca499828d3aa00dbaea4ccdf442dc
Abdul Qadhir <kaderchalil@Abduls-iMac.local>
2020-05-31 07:48:28 +0530

    To Server

---
+++ build/precache-manifest.0c8c83f74c884dc1fec6a8603d8f3070.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0821d16f217fba93b158",
+    "url": "/static/js/main.0821d16f.chunk.js"
+  },
+  {
+    "revision": "b22a2f3893ec6aebd650",
+    "url": "/static/css/1.a2c470ec.chunk.css"
+  },
+  {
+    "revision": "b22a2f3893ec6aebd650",
+    "url": "/static/js/1.b22a2f38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee91b2b5f2d82ad81d771f173fa77c7a",
+    "url": "/index.html"
+  }
+];

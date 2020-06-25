60f5ff3c6ec03087265ba2f877c08b4af10c4782
AbdulQadhir <kader.chalil@gmail.com>
2020-06-25 18:20:33 +0530

    tmp

---
+++ build/precache-manifest.5ca9fdd8c3bb533c47d0fac93e471b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36aae8d0bb1097469f7c",
+    "url": "/static/js/main.36aae8d0.chunk.js"
+  },
+  {
+    "revision": "5575cd1f4449476f46a5",
+    "url": "/static/css/1.064c6293.chunk.css"
+  },
+  {
+    "revision": "5575cd1f4449476f46a5",
+    "url": "/static/js/1.5575cd1f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "32caa8094692a8d9b473698c8fb0962a",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c0a5447012ae8c6bf350e456025f65b.js"
+  "/precache-manifest.5ca9fdd8c3bb533c47d0fac93e471b4c.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

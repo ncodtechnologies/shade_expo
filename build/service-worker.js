284686ad8057f7fa368be05fa933df4f35e49d88
AbdulQadhir <kader.chalil@gmail.com>
2020-09-07 13:15:26 +0530

    Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.237cbf93d7f5ed8060c87c2966aa3f6d.js"
+  "/precache-manifest.813b7dc513984ab35523b222ccf7b2b9.js"
 );
 
 workbox.clientsClaim();

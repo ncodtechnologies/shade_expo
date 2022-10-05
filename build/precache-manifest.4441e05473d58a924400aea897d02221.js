24c3c25016d4d10a44a450cfd7b8e5816cddc9ba
Abdul kader chalil <kader.chalil@gmail.com>
2022-10-05 20:26:55 +0530

    fix

---
+++ build/precache-manifest.4441e05473d58a924400aea897d02221.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1687bf2c8d118009446",
+    "url": "/static/js/main.e5191d67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9fbf898dbf44ae3dad98",
+    "url": "/static/css/2.33189564.chunk.css"
+  },
+  {
+    "revision": "9fbf898dbf44ae3dad98",
+    "url": "/static/js/2.68adc63a.chunk.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/static/media/loading.3f259006.gif"
+  },
+  {
+    "revision": "1cbdee9a458291a74e54e08c6a5558bf",
+    "url": "/index.html"
+  }
+];
